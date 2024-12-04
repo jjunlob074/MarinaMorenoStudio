e76e660425882adf9165131c7a9ab480cc7cdd43
jjunlob074 <jjunlob074@g.educaand.es>
2024-12-04 16:46:51 +0100

    first commit

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,23 +2,24 @@
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      black: '#000',
-      white: '#fff',
-      'dark-blue': '#022859',
-      'light-blue': '#F0F4F8',
-      'dark-gray': '#333333',
-      'light-gray': '#E0E0E0',
-      'steel-blue': '#B0C4DE',
-      'slate-gray': '#708090',
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        black: '#000',
+        white: '#fff',
+        'dark': '#E4DDCC',
+        'light-blue': '#F0F4F8',
+        'dark-gray': '#333333',
+        'light-gray': '#E0E0E0',
+        'steel-blue': '#B0C4DE',
+        'slate-gray': '#708090',
+      },
+      fontFamily: {
+        sans: ['Montserrat', 'sans-serif'],
+        serif: ['Playfair Display', 'serif'],
+      },
     },
-    fontFamily: {
-      sans: ['Montserrat', 'sans-serif'],
-      serif: ['Playfair Display', 'serif'],
-    },
-    extend: {},
   },
   plugins: [],
-}
+};
